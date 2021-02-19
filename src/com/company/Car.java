9453a8958bb6c7aa40b8aaa0dc35731623fa6750
student01 <giopolizia@gmail.com>
2021-02-19 13:45:02 +0400

    Initial commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Car {
+    private int topSpeed;
+    private int price;
+    private String carName;
+    private String color;
+
+    public int getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(int topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
